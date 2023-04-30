2c925e8ee92f84f307b12396cd0b834cf3f18661
TjaartNaude <tnaude@uh.edu>
2023-04-30 07:41:58 -0500

    Update clients.js

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -8,16 +8,18 @@ const { clients } = require('../models/models')
 
 // GET 10 most recent clients for org
 router.get('/', (req, res, next) => {
-  clients.find({ org: org }, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      console.log(data)
-      return res.json(data)
-    }
-  })
-  .sort({ updatedAt: -1 })
-  .limit(10)
+  const dbQuery = { orgs: org }
+  clients
+    .find(dbQuery, (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        console.log(data)
+        return res.json(data)
+      }
+    })
+    .sort({ updatedAt: -1 })
+    .limit(10)
 })
 
 // GET single client by ID
